05b1698e5c89ff34d855d2baefd499cd4863ffda
karanbir8872 <you@example.com>
2019-08-26 16:04:46 +0530

    add-files

--- src/components/picture.js
+++ src/components/picture.js
@@ -1,5 +1,10 @@
 import React from "react"
 const picture =()=>(
+    <div>
 <h1>hello friend</h1>
+<h1>hello friend</h1>
+<h1>hello friend</h1>
+<h1>hello friend</h1>
+</div>
 );
 export default picture;
